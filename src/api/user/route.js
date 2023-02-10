4fa40516c33511ff626ee1a9f1c87e986dc32ebc
NandLalgurjar <nlgurjar.img@gmail.com>
2023-02-10 10:25:33 +0530

    login-otp-verify route

--- src/api/user/route.js
+++ src/api/user/route.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const responseHandler = require("../../utils/responseHandlers")
-const { register, otpSent, otpVerify, login } = require('./controller');
+const { register, otpSent, otpVerify, login, loginOtpVerify } = require('./controller');
 const app = Router();
 
 app.post("/otp-sent", responseHandler(otpSent));
@@ -8,6 +8,7 @@ app.post("/otp-verify", responseHandler(otpVerify));
 app.post("/register", responseHandler(register));
 
 app.post("/login", responseHandler(login));
+app.post("/login-otp-verify", loginOtpVerify)
 
 
 
