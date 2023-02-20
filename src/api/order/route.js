69a277569a82eca60fb276fb190a334742d38d93
NandLalgurjar <nlgurjar.img@gmail.com>
2023-02-20 10:31:08 +0530

    add a fuction getServiceByCategory

---
+++ src/api/order/route.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const responseHandler = require("../../utils/responseHandlers")
+const auth = require("../../middleware/auth")
+const { userOrder } = require('./controller');
+const app = Router();
+
+app.get("/order", auth, responseHandler(userOrder))
+
+module.exports = app
