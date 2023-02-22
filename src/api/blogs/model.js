066f6b0e7e10c22f5f10c48972a88a82d39ed671
NandLalgurjar <nlgurjar.img@gmail.com>
2023-02-22 15:04:12 +0530

    modified :add 2 function getAllBlog && creatBlog

---
+++ src/api/blogs/model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Blog = new mongoose.Schema({
+    category: {
+        type: mongoose.Types.ObjectId,
+        default: null
+    },
+    Title: {
+        type: String,
+    },
+    image: {
+        type: [String],
+    },
+    WriterName: {
+        type: String,
+    },
+    WriteDate: {
+        type: Date,
+        default: Date()
+    },
+    Description: {
+        type: String,
+    }
+}, { timestamps: true });
+
+
+const blog = new mongoose.model("blog", Blog);
+module.exports = blog;
+
+
