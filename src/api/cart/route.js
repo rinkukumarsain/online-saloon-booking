86f3a89bcd8fb8f2287e1b86adcfe70b8ecf9aca
NandLalgurjar <nlgurjar.img@gmail.com>
2023-02-24 12:07:16 +0530

    modified : addcart

--- src/api/cart/route.js
+++ src/api/cart/route.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const responseHandler = require("../../utils/responseHandlers");
 const auth = require("../../middleware/auth");
-const { cartRegistration, editCart, removeServishFromCart, getcart } = require('./controller');
+const { cartRegistration, removeServishFromCart, getcart, addcart } = require('./controller');
 const app = Router();
 
 app.post("/cart-registration", auth, responseHandler(cartRegistration));
-app.post("/edit-Cart", auth, responseHandler(editCart));
+
 app.get("/remove-servish-from-cart", auth, responseHandler(removeServishFromCart));
 app.get("/get-cart", auth, responseHandler(getcart));
+app.post("/add-cart", auth, responseHandler(addcart))
 
 module.exports = app;
