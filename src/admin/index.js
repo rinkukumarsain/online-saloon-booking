65011173c9ce9934c16c731c2de4693e51be6b87
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-03 13:20:27 +0530

    admin merge

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,4 +1,4 @@
-const adminRoutes = require("./admin/route");
-const saloonstore = require("./saloonstore/route");
+const adminRoutes = require("./admin1/route");
+const saloonstore = require("./saloonStore1/route");
 
 module.exports = [adminRoutes,saloonstore]
