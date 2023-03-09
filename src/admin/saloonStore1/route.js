6f7f478f47198d8bb80b44535263ec49ad2cdb21
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-09 09:47:49 +0530

    change in viwes file

--- src/admin/saloonStore1/route.js
+++ src/admin/saloonStore1/route.js
@@ -3,14 +3,16 @@ const Upload = require("../../middleware/img");
 const responseHandler = require("../../utils/responseHandlers");
 const { Router } = require("express");
 const app = Router();
-const { saloonRegistration, addSaloon, add_Saloon_View, viewSaloon, getSaloons, saloonAllRequistDatatable } = require('./controller');
+const { saloonRegistration, addSaloon, add_Saloon_View, viewSaloon, getSaloonsDataTable, saloonAllRequistDatatable, saloonApproval } = require('./controller');
 
 app.get("/add-saloon", auth, addSaloon);
 app.get("/saloon-all-requist-datatable", auth, saloonAllRequistDatatable)
+app.get("/saloon-approval", auth, saloonApproval)
+
 app.get("/add_saloon_view", auth, add_Saloon_View);
 app.post("/saloon-registration", auth, saloonRegistration);
 app.get("/view-saloon", auth, viewSaloon);
-app.get("/get-saloons", auth, getSaloons)
+app.get("/get-saloons-dataTable", auth, getSaloonsDataTable)
 
 
 
