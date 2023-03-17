4da990dcaeadf8196a95448cb3432216246ac63a
sahilimg <sahilagarwal.img@gmail.com>
2023-03-17 18:56:22 +0530

    sahil

---
+++ src/admin/add_category/model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const add_Category = new mongoose.Schema({
+    parent_Name: {
+        type: mongoose.Types.ObjectId,
+        default: null
+    },
+    Name: {
+        type: String,
+    },
+    image: {
+        type: String,
+    }
+}, { timestamps: true });
+
+
+const category = new mongoose.model("categorys", add_Category);
+module.exports = category;
+
+
