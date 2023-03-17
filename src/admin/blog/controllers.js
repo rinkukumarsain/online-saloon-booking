e6b66f5fc6a5c0492378e3e4a5104cf06e619019
Punit <Punit>
2023-03-17 18:40:11 +0530

    Blog Modules

---
+++ src/admin/blog/controllers.js
@@ -0,0 +1,83 @@
+const category = require("../../api/category/model");
+const blog = require("../../api/blogs/model");
+const mongoose = require("mongoose")
+const service = require("./services")
+
+exports.ADD_BLOG = async (req, res) => {
+    const category_data = await category.find({ parent_Name: null })
+    const _id = req.query.id
+    const user = req.user
+    const blog_data = await blog.findOne({_id})
+    res.render("blog/add_blog",{user,category_data,_id,blog_data})
+}
+
+exports.ADD_BLOG_STORE = async (req, res) => {
+    try {
+        let { body, file, query } = req
+        console.log("--->", req.body)
+        console.log("==>", req.file)
+        console.log(req.query)
+        res.locals.message = req.flash();
+        if (query.id) {
+
+            let _id = mongoose.Types.ObjectId(query.id);
+            const result = await blog.findOne({ _id });
+            if (result) {
+                let obj = {};
+                    if (body.category) { obj.category = body.category };
+                    if (body.Title) { obj.Title = body.Title };
+                    if (body.WriteDate) { obj.WriteDate = body.WriteDate };
+                    if (body.WriterName) { obj.WriterName = body.WriterName };
+                    if (body.Description) { obj.Description = body.Description };
+                     if (file.length>0) {
+                        img = []
+                            img.push(file.filename)
+                        obj.image = img
+                    }
+                    const result = await blog.findByIdAndUpdate({ _id }, { $set: obj }, { new: true });
+                    if (result) {
+                        req.flash("success", "Blog  is  Update successfull !")
+                        res.redirect("/")
+                    };
+               
+            } else {
+                req.flash("error", "Blog is Not Found !")
+                res.redirect("/")
+            };
+        } else {
+        
+            if (file) {
+                img = []
+                img.push(file.filename)
+                body.image = img
+            } else {
+                body.image = ""
+            }
+            let blog_details = new blog({
+                Title: body.Title,
+                WriterName: body.WriterName,
+                WriteDate: body.WriteDate,
+                Description: body.description,
+                image: body.image,
+                category:  mongoose.Types.ObjectId(body.category)
+            });
+            const result = await blog_details.save();
+            res.send()
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+exports.VIEW_BLOG = async (req, res) => {
+    const user = req.user
+    const data = await service.VIEW_BLOG()
+    res.render("blog/view_blog",{data,user})
+}
+
+exports.DELETE_BLOG = async (req, res) => { 
+    const id = req.query.id
+    await blog.findByIdAndDelete({ _id: id })
+    res.redirect("/view_blog")
+}
---
+++ src/admin/blog/routes.js
@@ -0,0 +1,13 @@
+const auth = require("../../middleware/adminauth")
+const Upload = require("../../middleware/img");
+const { Router } = require("express");
+const app = Router();
+const {ADD_BLOG ,ADD_BLOG_STORE,VIEW_BLOG,DELETE_BLOG} = require('./controllers');
+app.get("/add_blog",auth,ADD_BLOG)
+
+app.post("/add_blog_store", auth, Upload.single("image"), ADD_BLOG_STORE)
+
+app.get("/view_blog", auth, VIEW_BLOG)
+app.get("/delete_blog", auth, DELETE_BLOG)
+
+module.exports = app
