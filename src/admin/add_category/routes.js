7baa3e409f82a112c660e341165b583c5a465468
sahilimg <sahilagarwal.img@gmail.com>
2023-03-20 18:34:09 +0530

    sahil

--- src/admin/add_category/routes.js
+++ src/admin/add_category/routes.js
@@ -2,7 +2,7 @@ const auth = require("../../middleware/adminauth")
 const Upload = require("../../middleware/img");
 const { Router } = require("express");
 const app = Router();
-const {Category, AddCategory ,ViwesCategory,ViwesCategoryes,edit_category,delete_category} = require("./controller");
+const {Category, AddCategory ,ViwesCategory,ViwesCategoryes,edit_category,delete_category,sub_category,add_sub_category,viwes_sub_category,viwes_sub_categorys} = require("./controller");
 
 
 //category
@@ -10,11 +10,16 @@ app.get("/category",auth, Category)
 app.post("/add_category",auth, Upload.single("file"), AddCategory)
 // app.get("/view-category", ViwesCategory)
 app.get("/view-category/:id?",auth, ViwesCategory)
-app.get("/viwes_categoryes/:id?",auth, ViwesCategoryes)
+//app.get("/viwes_categoryes/:id?",auth, ViwesCategoryes)
 
 // edit delete
-app.get("/edit/:id",auth, edit_category)
-app.get("/delete/:id",auth, delete_category)
+app.get("/edit",auth, edit_category)
+app.get("/delete",auth, delete_category)
+app.get("/sub_category/:id?",auth, sub_category)
+app.post("/add_sub-category", auth,Upload.single("file"), add_sub_category)
+app.get("/view_sub_category/:id?",auth, viwes_sub_category)
+app.get("/viwes_sub_categorys/:id?",auth, viwes_sub_categorys)
+
 
 // app.get("/pages-register", register);
 // app.post("/register-admin-data", adminRegisterData);
