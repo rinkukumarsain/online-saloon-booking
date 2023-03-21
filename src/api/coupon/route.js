a5b68396d4d8556c2a8ca1998aa508d150f9e519
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-21 09:24:18 +0530

    get-coupon

--- src/api/coupon/route.js
+++ src/api/coupon/route.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const responseHandler = require("../../utils/responseHandlers");
-const auth = require("../../middleware/adminauth");
+const auth = require("../../middleware/auth");
 const { createCoupon, editCoupon, getCoupon } = require('./controller');
 const app = Router();
 const { joi_createCoupon, joi_EditCoupon } = require("../../middleware/joi_createCoupon")
