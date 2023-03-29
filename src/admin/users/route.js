498be2ed109be89216b17d9106ca6ea3581bbad5
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-29 14:15:34 +0530

    modified in warningPage and send mail to admin fuction

--- src/admin/users/route.js
+++ src/admin/users/route.js
@@ -1,15 +1,15 @@
 const auth = require("../../middleware/adminauth");
 const { Router } = require("express");
 const app = Router();
-const { allUser, BlockUser, warning,warningPage} = require('./controller');
+const { allUser, BlockUser, warning, warningPage } = require('./controller');
 
 const { joi_createCoupon } = require("../../middleware/joi_createCoupon");
 
 app.get("/all-user", auth, allUser)
 
 app.get("/Block-User", auth, BlockUser)
-app.get("/warningpage",auth,warningPage)
-app.post("/warning",auth,warning)
+app.get("/warningpage", auth, warningPage)
+app.post("/warning", auth, warning)
 
 
 module.exports = app;
