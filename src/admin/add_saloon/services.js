cd5023645b59988ff165e2306df20dc1f5e988af
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-30 18:37:20 +0530

    modifyed in admin panal like redirect

--- src/admin/add_saloon/services.js
+++ src/admin/add_saloon/services.js
@@ -1,7 +1,28 @@
+const { default: mongoose } = require("mongoose");
 const saloon = require("../../api/saloonstore/model");
 
-exports.VIEW_SALOON = async () => {
-  let pipeline = []
+exports.VIEW_SALOON = async (req) => {
+
+  let match = {}
+  if (req.query.city != undefined && req.query.city != "") {
+    match['location.city'] = req.query.city
+  }
+  if (req.query.Phone != undefined && req.query.Phone != "") {
+    match.Phone = { $eq: Number(req.query.Phone) }
+  }
+  if (req.query.email != undefined && req.query.email != "") {
+    match.email = { $regex: req.query.email, $options: 'i' }
+  }
+  if (req.query.userId != undefined && req.query.userId != "") {
+    match.userId = mongoose.Types.ObjectId(req.query.userId)
+  }
+
+  let pipeline = [];
+  pipeline.push({
+    '$match': match
+  })
+
+
   pipeline.push({
     '$lookup': {
       'from': 'users',
--- src/admin/order/services.js
+++ src/admin/order/services.js
@@ -10,6 +10,9 @@ exports.getAllOrder = async (req) => {
         if (req.query.userId != undefined && req.query.userId != "") {
             match.userId = mongoose.Types.ObjectId(req.query.userId)
         }
+        if (req.query.orderId != undefined && req.query.orderId != "") {
+            match._id = mongoose.Types.ObjectId(req.query.orderId)
+        }
         if (req.query.status != undefined && req.query.status != "") {
             match.status = req.query.status
         }
