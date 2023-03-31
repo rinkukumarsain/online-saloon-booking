96a2b0b1b1a40d96275f3e12e22af61c43de623c
NandLalgurjar <nlgurjar.img@gmail.com>
2023-03-31 17:25:14 +0530

    Artists curd

---
+++ src/admin/Artists/routes.js
@@ -0,0 +1,10 @@
+const auth = require("../../middleware/adminauth")
+const Upload = require("../../middleware/img");
+const { Router } = require("express");
+const app = Router();
+const { Artists,  } = require('./controllers');
+
+app.get("/Artists", auth, Artists)
+// app.get("/Find-Artists", auth, )
+
+module.exports = app
