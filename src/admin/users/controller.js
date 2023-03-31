ef55e29dffd01d6b06afcfe707ca13797b60873f
sahilimg <sahilagarwal.img@gmail.com>
2023-03-31 11:35:29 +0530

    sahil

--- src/admin/users/controller.js
+++ src/admin/users/controller.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const { getAllSaloonCity } = require("../../api/saloonstore/controller");
 const moment = require("moment")
 const { allUser } = require("./services")
-const { sendmailwarning } = require("../../middleware/mail")
+const { sendmailwarning } = require("../../middleware/mail");
+const { findOne } = require("../../api/user/model");
 
 exports.allUser = async (req, res) => {
     try {
@@ -16,9 +17,20 @@ exports.allUser = async (req, res) => {
 };
 
 exports.BlockUser = async (req, res) => {
-    try {
+    try {res.locals.message = req.flash();
         // console.log("re", req.query.id)
-        const Finddata = await user.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.query.id) }, { type: "block-User" }, { new: true })
+        let Finddata;
+        const data=await user.findOne({_id:mongoose.Types.ObjectId(req.query.id)});
+        if(data.type=="user")
+        {
+         Finddata = await user.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.query.id) }, { type: "block-User" }, { new: true })
+        }
+        else
+        {
+             Finddata = await user.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.query.id) }, { type: "block-admin" }, { new: true })
+
+        }
+        req.flash("success","block successfully")
         if (Finddata) {
             res.redirect("/all-user")
         }
@@ -48,6 +60,27 @@ exports.warning = async (req, res) => {
             res.redirect("/all-user")
         } else {
             req.flash("error", "mail not Send")
+            res.redirect("/all-user")
+        }
+    } catch (error) {
+        console.log(error);
+    };
+};
+exports.unblock = async (req, res) => {
+    try {let Finddata;
+        res.locals.message = req.flash();
+        const data=await user.findOne({_id:mongoose.Types.ObjectId(req.query.id)});
+        if(data.type=="user")
+        {
+         Finddata = await user.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.query.id) }, { type: "user" }, { new: true })
+        }
+        else
+        {
+             Finddata = await user.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.query.id) }, { type: "admin" }, { new: true })
+
+        }
+        req.flash("success","unblock successfully")
+        if (Finddata) {
             res.redirect("/all-user")
         }
     } catch (error) {
