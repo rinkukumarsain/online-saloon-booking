1513c03128930b2bef2f6a0f596eb30877571622
NandLalgurjar <nlgurjar.img@gmail.com>
2023-04-13 17:04:21 +0530

    ViwesFindQustion

--- src/admin/add_frequent/controller.js
+++ src/admin/add_frequent/controller.js
@@ -1,50 +1,62 @@
 const faqModel = require("./model")
+const mongoose = require("mongoose")
 
 exports.ADD_FREQUENT = async (req, res) => {
+    try {
+        const user = req.user;
+        const _id = req.query.id;
+        const faqData = await faqModel.findOne({ _id });
+        res.render("add_frequent/add_frequent", { user, faqData });
+    } catch (err) {
+        console.log(err);
+    };
+};
 
-    const user = req.user
-    const _id = req.query.id
-    const faqData = await faqModel.findOne({ _id })
-
-    res.render("add_frequent/add_frequent", { user, faqData })
-}
 exports.DELETE_FREQUENT = async (req, res) => {
-    const id = req.query.id;
-
-    const data = await faqModel.findByIdAndDelete({ _id: id })
-
-    res.redirect("/view_frequent")
-}
+    try {
+        const id = req.query.id;
+        const data = await faqModel.findByIdAndDelete({ _id: id });
+        res.redirect("/view_frequent");
+    } catch (err) {
+        console.log(err);
+    };
+};
 
 exports.VIEW_FREQUENT = async (req, res) => {
-    const data = await faqModel.find()
-    const user = req.user
-    res.render("add_frequent/view_frequent", { user, data })
+    try {
+        const data = await faqModel.find();
+        const user = req.user;
+        res.render("add_frequent/view_frequent", { user, data });
+    } catch (err) {
+        console.log(err);
+    };
 }
 exports.ADD_FREQUENT_DATA = async (req, res) => {
-    res.locals.message = req.flash();
-    //const {answer,question} = req.query;
-    const { answer, question } = req.body;
-    //console.log(req.body)
-    //answer=answer.slice(1,answer.length-1)
-    //question=question.slice(1,question.length-1)
-    // console.log("ans",ans,"que",que)
-    // console.log(answer);
-    // // console.log(question.toString());
-    if (req.body.faqData) {
-        req.flash("success", "Question's Answer update succesfully")
-        //  console.log("faqData",req.body.faqData)
-        //  console.log("hiinbkbkvhv")
-        const faqData = req.body.faqData;
-        const model = await faqModel.findByIdAndUpdate({ _id: faqData }, { answer: answer, question: question });
-        res.send();
-    }
-    else {
-        req.flash("success", "Question's Answer update succesfully")
-        const model = new faqModel({ answer: answer, question: question });
-        await model.save();
-        // console.log("hii");
-        res.send();
-    }
-}
+    try {
+        res.locals.message = req.flash();
+        const { answer, question } = req.body;
+        if (req.body.faqData) {
+            req.flash("success", "Question's Answer update succesfully");
+            const faqData = req.body.faqData;
+            const model = await faqModel.findByIdAndUpdate({ _id: faqData }, { answer: answer, question: question });
+            res.send();
+        } else {
+            req.flash("success", "Question's Answer update succesfully");
+            const model = new faqModel({ answer: answer, question: question });
+            await model.save();
+            res.send();
+        }
+    } catch (err) {
+        console.log(err);
+    };
+};
+
+exports.ViwesFindQustion = async (req, res) => {
+    try {
+        const data = await faqModel.find({ _id: mongoose.Types.ObjectId(req.query.id) });
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+    };
+};
 
--- src/admin/add_frequent/routes.js
+++ src/admin/add_frequent/routes.js
@@ -1,20 +1,14 @@
-const auth = require("../../middleware/adminauth")
+const auth = require("../../middleware/adminauth");
 const Upload = require("../../middleware/img");
 const { Router } = require("express");
 const app = Router();
-const { ADD_FREQUENT, ADD_FREQUENT_DATA, VIEW_FREQUENT, VIEW_FREQUENT_DATA, DELETE_FREQUENT } = require("./controller");
+const { ADD_FREQUENT, ADD_FREQUENT_DATA, VIEW_FREQUENT, DELETE_FREQUENT, ViwesFindQustion } = require("./controller");
 
 app.get("/add_frequent", auth, ADD_FREQUENT)
 app.post("/addfrequentdata", auth, ADD_FREQUENT_DATA)
 app.get("/view_frequent", auth, VIEW_FREQUENT)
 app.get("/delete_frequent", auth, DELETE_FREQUENT)
+app.get("/Viwes-Find-Qustion", auth, ViwesFindQustion)
 
-
-
-// app.post("/add_service_store", auth, Upload.array("image"), ADD_SERVICE_STORE)
-
-// app.get("/view_service", auth, VIEW_SERVICE)
-
-// app.get("/delete_service",DELETE_SERVICE)
 
 module.exports = app
