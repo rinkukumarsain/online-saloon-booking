c562020d3f44acc6df5d6694ebd7db73a0335961
NandLalgurjar <nlgurjar.img@gmail.com>
2023-04-18 16:32:02 +0530

    create a function deletReferEranProgram

--- src/admin/Refer-And-point/routes.js
+++ src/admin/Refer-And-point/routes.js
@@ -2,11 +2,12 @@ const auth = require("../../middleware/adminauth");
 const Upload = require("../../middleware/img");
 const { Router } = require("express");
 const app = Router();
-const { setPointToRupee, pointAndRupee, ViewReferEarn } = require('./controllers');
+const { setPointToRupee, pointAndRupee, ViewReferEarn, deletReferEranProgram } = require('./controllers');
 
 app.get("/Set-point-to-rupee", auth, setPointToRupee);
 app.post("/Set-Point-To-Rupee", auth, pointAndRupee);
 app.get("/View-Refer-Earn", auth, ViewReferEarn);
+app.get("/Delete-Refer-Eran-program", auth, deletReferEranProgram);
 
 
 
